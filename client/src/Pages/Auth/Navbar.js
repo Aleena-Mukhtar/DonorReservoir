370415e7eb4662bd182bf6a6bd4f5db42c027059
Aleena-Mukhtar <aleenamukhtar15@gmail.com>
2022-12-30 17:29:47 +0500

    practice session

--- client/src/Pages/Auth/Navbar.js
+++ client/src/Pages/Auth/Navbar.js
@@ -4,6 +4,10 @@ export default function Navbar() {
   return (
     <div className='navbar'>
       Navbar
+      <img class="Logo" src={process.env.PUBLIC_URL + '/patient-image.jpg'} alt="logo" /> 
+      <div className='navClass'>
+        Logo
+      </div>
     </div>
   )
 }
