cc9ae82ce895bed970f803e5bd3cfd89a6ee160f
Aleena-Mukhtar <aleenamukhtar15@gmail.com>
2023-03-16 20:04:34 +0500

    admin Dashboard all buttons and modals done

--- client/src/Pages/Admin/AdminDashboard.js
+++ client/src/Pages/Admin/AdminDashboard.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
 import { AiOutlineLogout } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
 
 export default function AdminDashboard() {
-  const [btnClick, setBtnClick] = useState(false);
+  const [btnClick, setBtnClick] = useState(2);
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
+  const navigateToEachPatientPage = () => {
+    navigate(`/eachPatient`);
+  };
+  const navigateToEachBankPage = () => {
+    navigate(`/eachBank`);
+  };
+  const navigateToBottleStockPage = () => {
+    navigate(`/bottlesStock`);
+  };
   return (
     <div className='adminDashboard'>
       <div className = "heading">Admin Dashboard</div>
       <div className='mainContent'>
         <div className="leftPanel">
           <div className='BtnDiv'>
-            <button className={btnClick ? 'btn click' : 'btn'} onClick={(e) => setBtnClick(!btnClick)}>Details of Bottles</button>
-            <button className={btnClick ? 'btn' : 'btn click'} onClick={(e) => setBtnClick(!btnClick)}>History</button>     
+            <button className={btnClick === 1 ? 'btn click' : 'btn'} onClick={navigateToBottleStockPage}>Details of Bottles</button>
+            <button className={btnClick === 2 ? 'btn click' : 'btn'} onClick={(e) => setBtnClick(2)}>Patient Requests</button>
+            <button className={btnClick === 3 ? 'btn click' : 'btn'} onClick={(e) => setBtnClick(3)}>Blood Bank</button>     
           </div>
-          <button className='logoutBtn'>
+          <button className='logoutBtn' onClick={(e) => setShowModal(!showModal)}>
             <AiOutlineLogout className='icon'/>
             <div className='label'>Log Out</div>
           </button>
@@ -33,120 +46,155 @@ export default function AdminDashboard() {
           <div className='tableCon'>
             <table className='table'>
               <thead className='tableHeader'>
-                <th className='headText' align='center'>Donor ID</th>
-                <th className='headText' align='center'>Name</th>
-                <th className='headText' align='center'>CNIC</th>
-                <th className='headText' align='center'>Blood Type</th>
-                <th className='headText' align='center'>Date</th>
+                <th className='headText' align='left'>
+                  {
+                    btnClick === 2 ? 'Patient Blood Requests' : 'Blood Bank Notifications'
+                  }
+                </th>
               </thead>
               <tbody className='tableBody'>
-                <tr className='eachRow'>
-                  <td className='rowText' align='center'>123456</td>
-                  <td className='rowText' align='center'>Aleena</td>
-                  <td className='rowText' align='center'>35202-1234567-0</td>
-                  <td className='rowText' align='center'>O+</td>
-                  <td className='rowText' align='center'>20 December, 2023</td>
+                <tr className='eachRow' onClick={btnClick === 2 ? navigateToEachPatientPage : navigateToEachBankPage}>
+                  <td className='rowText' align='center'>
+                    <div>
+                      {
+                        btnClick === 2 ? 'Patient with ID 123456 Requested for Blood Bottle(s)' : 'You got a reply From Aleena Donations Bank'
+                      }
+                    </div>
+                    <div className='detailsCon'>
+                      <div className='date'>20 December, 2022</div>
+                      <div className='time'>8:50 pm</div>
+                    </div>
+                  </td>
                 </tr>
-                <tr className='eachRow'>
-                  <td className='rowText' align='center'>123456</td>
-                  <td className='rowText' align='center'>Alishbah</td>
-                  <td className='rowText' align='center'>35202-1234567-0</td>
-                  <td className='rowText' align='center'>O+</td>
-                  <td className='rowText' align='center'>20 December, 2023</td>
+                <tr className='eachRow' onClick={btnClick === 2 ? navigateToEachPatientPage : navigateToEachBankPage}>
+                  <td className='rowText' align='center'>
+                    <div>
+                      {
+                        btnClick === 2 ? 'Patient with ID 123456 Requested for Blood Bottle(s)' : 'You got a reply From Aleena Donations Bank'
+                      }
+                    </div>
+                    <div className='detailsCon'>
+                      <div className='date'>20 December, 2022</div>
+                      <div className='time'>8:50 pm</div>
+                    </div>
+                  </td>
                 </tr>
-                <tr className='eachRow'>
-                  <td className='rowText' align='center'>123456</td>
-                  <td className='rowText' align='center'>Maniha</td>
-                  <td className='rowText' align='center'>35202-1234567-0</td>
-                  <td className='rowText' align='center'>O+</td>
-                  <td className='rowText' align='center'>20 December, 2023</td>
+                <tr className='eachRow' onClick={btnClick === 2 ? navigateToEachPatientPage : navigateToEachBankPage}>
+                  <td className='rowText' align='center'>
+                    <div>
+                      {
+                        btnClick === 2 ? 'Patient with ID 123456 Requested for Blood Bottle(s)' : 'You got a reply From Aleena Donations Bank'
+                      }
+                    </div>
+                    <div className='detailsCon'>
+                      <div className='date'>20 December, 2022</div>
+                      <div className='time'>8:50 pm</div>
+                    </div>
+                  </td>
                 </tr>
-                <tr className='eachRow'>
-                  <td className='rowText' align='center'>123456</td>
-                  <td className='rowText' align='center'>Tanzeela</td>
-                  <td className='rowText' align='center'>35202-1234567-0</td>
-                  <td className='rowText' align='center'>O+</td>
-                  <td className='rowText' align='center'>20 December, 2023</td>
+                <tr className='eachRow' onClick={btnClick === 2 ? navigateToEachPatientPage : navigateToEachBankPage}>
+                  <td className='rowText' align='center'>
+                    <div>
+                      {
+                        btnClick === 2 ? 'Patient with ID 123456 Requested for Blood Bottle(s)' : 'You got a reply From Aleena Donations Bank'
+                      }
+                    </div>
+                    <div className='detailsCon'>
+                      <div className='date'>20 December, 2022</div>
+                      <div className='time'>8:50 pm</div>
+                    </div>
+                  </td>
                 </tr>
-                <tr className='eachRow'>
-                  <td className='rowText' align='center'>123456</td>
-                  <td className='rowText' align='center'>Mahnoor</td>
-                  <td className='rowText' align='center'>35202-1234567-0</td>
-                  <td className='rowText' align='center'>O+</td>
-                  <td className='rowText' align='center'>20 December, 2023</td>
+                <tr className='eachRow' onClick={btnClick === 2 ? navigateToEachPatientPage : navigateToEachBankPage}>
+                  <td className='rowText' align='center'>
+                    <div>
+                      {
+                        btnClick === 2 ? 'Patient with ID 123456 Requested for Blood Bottle(s)' : 'You got a reply From Aleena Donations Bank'
+                      }
+                    </div>
+                    <div className='detailsCon'>
+                      <div className='date'>20 December, 2022</div>
+                      <div className='time'>8:50 pm</div>
+                    </div>
+                  </td>
                 </tr>
-                <tr className='eachRow'>
-                  <td className='rowText' align='center'>123456</td>
-                  <td className='rowText' align='center'>Aleena</td>
-                  <td className='rowText' align='center'>35202-1234567-0</td>
-                  <td className='rowText' align='center'>O+</td>
-                  <td className='rowText' align='center'>20 December, 2023</td>
+                <tr className='eachRow' onClick={btnClick === 2 ? navigateToEachPatientPage : navigateToEachBankPage}>
+                  <td className='rowText' align='center'>
+                    <div>
+                      {
+                        btnClick === 2 ? 'Patient with ID 123456 Requested for Blood Bottle(s)' : 'You got a reply From Aleena Donations Bank'
+                      }
+                    </div>
+                    <div className='detailsCon'>
+                      <div className='date'>20 December, 2022</div>
+                      <div className='time'>8:50 pm</div>
+                    </div>
+                  </td>
                 </tr>
-                <tr className='eachRow'>
-                  <td className='rowText' align='center'>123456</td>
-                  <td className='rowText' align='center'>Alishbah</td>
-                  <td className='rowText' align='center'>35202-1234567-0</td>
-                  <td className='rowText' align='center'>O+</td>
-                  <td className='rowText' align='center'>20 December, 2023</td>
+                <tr className='eachRow' onClick={btnClick === 2 ? navigateToEachPatientPage : navigateToEachBankPage}>
+                  <td className='rowText' align='center'>
+                    <div>
+                      {
+                        btnClick === 2 ? 'Patient with ID 123456 Requested for Blood Bottle(s)' : 'You got a reply From Aleena Donations Bank'
+                      }
+                    </div>
+                    <div className='detailsCon'>
+                      <div className='date'>20 December, 2022</div>
+                      <div className='time'>8:50 pm</div>
+                    </div>
+                  </td>
                 </tr>
-                <tr className='eachRow'>
-                  <td className='rowText' align='center'>123456</td>
-                  <td className='rowText' align='center'>Maniha</td>
-                  <td className='rowText' align='center'>35202-1234567-0</td>
-                  <td className='rowText' align='center'>O+</td>
-                  <td className='rowText' align='center'>20 December, 2023</td>
+                <tr className='eachRow' onClick={btnClick === 2 ? navigateToEachPatientPage : navigateToEachBankPage}>
+                  <td className='rowText' align='center'>
+                    <div>
+                      {
+                        btnClick === 2 ? 'Patient with ID 123456 Requested for Blood Bottle(s)' : 'You got a reply From Aleena Donations Bank'
+                      }
+                    </div>
+                    <div className='detailsCon'>
+                      <div className='date'>20 December, 2022</div>
+                      <div className='time'>8:50 pm</div>
+                    </div>
+                  </td>
                 </tr>
-                <tr className='eachRow'>
-                  <td className='rowText' align='center'>123456</td>
-                  <td className='rowText' align='center'>Tanzeela</td>
-                  <td className='rowText' align='center'>35202-1234567-0</td>
-                  <td className='rowText' align='center'>O+</td>
-                  <td className='rowText' align='center'>20 December, 2023</td>
+                <tr className='eachRow' onClick={btnClick === 2 ? navigateToEachPatientPage : navigateToEachBankPage}>
+                  <td className='rowText' align='center'>
+                    <div>
+                      {
+                        btnClick === 2 ? 'Patient with ID 123456 Requested for Blood Bottle(s)' : 'You got a reply From Aleena Donations Bank'
+                      }
+                    </div>
+                    <div className='detailsCon'>
+                      <div className='date'>20 December, 2022</div>
+                      <div className='time'>8:50 pm</div>
+                    </div>
+                  </td>
                 </tr>
-                <tr className='eachRow'>
-                  <td className='rowText' align='center'>123456</td>
-                  <td className='rowText' align='center'>Mahnoor</td>
-                  <td className='rowText' align='center'>35202-1234567-0</td>
-                  <td className='rowText' align='center'>O+</td>
-                  <td className='rowText' align='center'>20 December, 2023</td>
-                </tr>
-                <tr className='eachRow'>
-                  <td className='rowText' align='center'>123456</td>
-                  <td className='rowText' align='center'>Aleena</td>
-                  <td className='rowText' align='center'>35202-1234567-0</td>
-                  <td className='rowText' align='center'>O+</td>
-                  <td className='rowText' align='center'>20 December, 2023</td>
-                </tr>
-                <tr className='eachRow'>
-                  <td className='rowText' align='center'>123456</td>
-                  <td className='rowText' align='center'>Alishbah</td>
-                  <td className='rowText' align='center'>35202-1234567-0</td>
-                  <td className='rowText' align='center'>O+</td>
-                  <td className='rowText' align='center'>20 December, 2023</td>
-                </tr>
-                <tr className='eachRow'>
-                  <td className='rowText' align='center'>123456</td>
-                  <td className='rowText' align='center'>Maniha</td>
-                  <td className='rowText' align='center'>35202-1234567-0</td>
-                  <td className='rowText' align='center'>O+</td>
-                  <td className='rowText' align='center'>20 December, 2023</td>
-                </tr>
-                <tr className='eachRow'>
-                  <td className='rowText' align='center'>123456</td>
-                  <td className='rowText' align='center'>Tanzeela</td>
-                  <td className='rowText' align='center'>35202-1234567-0</td>
-                  <td className='rowText' align='center'>O+</td>
-                  <td className='rowText' align='center'>20 December, 2023</td>
-                </tr>
-                <tr className='eachRow'>
-                  <td className='rowText' align='center'>123456</td>
-                  <td className='rowText' align='center'>Mahnoor</td>
-                  <td className='rowText' align='center'>35202-1234567-0</td>
-                  <td className='rowText' align='center'>O+</td>
-                  <td className='rowText' align='center'>20 December, 2023</td>
+                <tr className='eachRow' onClick={btnClick === 2 ? navigateToEachPatientPage : navigateToEachBankPage}>
+                  <td className='rowText' align='center'>
+                    <div>
+                      {
+                        btnClick === 2 ? 'Patient with ID 123456 Requested for Blood Bottle(s)' : 'You got a reply From Aleena Donations Bank'
+                      }
+                    </div>
+                    <div className='detailsCon'>
+                      <div className='date'>20 December, 2022</div>
+                      <div className='time'>8:50 pm</div>
+                    </div>
+                  </td>
                 </tr>
               </tbody>
             </table>
+          </div>
+        </div>
+      </div>
+      <div className='logoutModal' style={{display: showModal ? 'flex' : 'none'}} onClick={(e) => setShowModal(false)}>
+        <div className='logout'>
+          <div className='modalHeading'>Confirm Logout</div>
+          <div className='innerHeading'>Are you sure you want to logout?</div>
+          <div className='btnCon'>
+            <button className='cancelBtn' onClick={(e) => setShowModal(false)}>Cancel</button>
+            <button className='okBtn'>OK</button>
           </div>
         </div>
       </div>
