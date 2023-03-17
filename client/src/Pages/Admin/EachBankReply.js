c6d8354d3da06635bdb9870956ded84814aaa640
Aleena-Mukhtar <aleenamukhtar15@gmail.com>
2023-03-18 00:22:21 +0500

    design changed

--- client/src/Pages/Admin/EachBankReply.js
+++ client/src/Pages/Admin/EachBankReply.js
@@ -11,78 +11,91 @@ export default function EachBankReply() {
     const [starred, setStarred] = useState(false);
     const [status, setStatus] = useState('notSet');
   return (
-    <div className='eachBankReply'>
+    <div className='eachPatientRequest eachBankReply'>
         <button className='backBtn' onClick={(e) => navigate(-1)}>
             <HiOutlineArrowNarrowLeft className='icon'/>
         </button>
         <div className='heading'>Blood Bank Details</div>
-        <div className='BtnCon'>
-            <button className='starBtn' style={{backgroundColor: starred ? '#b81d1d' : '#FFFFFF'}} onClick={(e) => setStarred(!starred)}>
-                <BsFillStarFill className='icon' style={{color: starred ? '#FFFFFF' : '#b81d1d'}}/>
-            </button>
-            <button className='deleteBtn' onClick={(e) => setShowDeleteModal(!showDeleteModal)}>
-                <MdDelete className='icon'/>
-            </button>
-        </div>
-        <div className='imgDiv'>
-            <img
-                src={process.env.PUBLIC_URL + "/ProfileLogo.PNG"}
-                alt="logo"
-                className="patient-img"
-            />
-        </div>
-        <div className='DetailCon'>
-            <div className='header'>Bank ID:</div>
-            <div className='text'>123456</div>
-        </div>
-        <div className='DetailCon'>
-            <div className='header'>Date:</div>
-            <div className='text'>22 December, 2023</div>
-        </div>
-        <div className='DetailCon'>
-            <div className='header'>Time:</div>
-            <div className='text'>8:50 pm</div>
-        </div>
-        <div className='DetailCon'>
-            <div className='header'>Bank Name:</div>
-            <div className='text'>First Last Name</div>
-        </div>
-        <div className='DetailCon'>
-            <div className='header'>Bank Address:</div>
-            <div className='text'>House # 12, Street # 34 Block A Lahore Punjab</div>
-        </div>
-        <div className='DetailCon'>
-            <div className='header'>Bank Admin Name:</div>
-            <div className='text'>First Second Name</div>
-        </div>
-        <div className='DetailCon'>
-            <div className='header'>Bank Admin CNIC:</div>
-            <div className='text'>35202-12345678-0</div>
-        </div>
-        <div className='DetailCon'>
-            <div className='header'>Phone Number 1: </div>
-            <div className='text'>12345678910</div>
-        </div>
-        <div className='DetailCon'>
-            <div className='header'>Phone Number 2: </div>
-            <div className='text'>12345678910</div>
-        </div>
-        <div className='DetailCon'>
-            <div className='header'>Email: </div>
-            <div className='text'>bank@gmail.com</div>
-        </div>
-        {
-            status === 'reject' ? 
+        <div className='MainContent'>
+            <div className='leftPanel'>
+                <div className='imgDiv'>
+                    <img
+                        src={process.env.PUBLIC_URL + "/ProfileLogo.PNG"}
+                        alt="logo"
+                        className="patient-img"
+                    />
+                </div>
+                <div className='BtnCon'>
+                    <button className='starBtn btn' style={{backgroundColor: starred ? '#b81d1d' : '#FFFFFF'}} onClick={(e) => setStarred(!starred)}>
+                        <BsFillStarFill className='icon' style={{color: starred ? '#FFFFFF' : '#b81d1d'}}/>
+                    </button>
+                    <button className='deleteBtn btn' onClick={(e) => setShowDeleteModal(!showDeleteModal)}>
+                        <MdDelete className='icon'/>
+                    </button>
+                    {
+                        status === 'notSet' ?
+                        <>
+                            <button className='RBtn denyBtn btn1' onClick={(e) => setShowDenyModal(!showDenyModal)}>Reject</button>
+                            <button className='ABtn btn1' onClick={(e) => setStatus('accept')}>Accept</button>
+                        </> : null
+                    }
+                </div>
+            </div>
+            <div className='rightPanel'>
             <div className='DetailCon'>
-                <div className='header'>Request Status: </div>
-                <div className='text Dstatus'>Rejected</div>
-            </div> :
-            status === 'accept' ? 
+                <div className='header'>Bank ID:</div>
+                <div className='text'>123456</div>
+            </div>
             <div className='DetailCon'>
-                <div className='header'>Request Status: </div>
-                <div className='text Astatus'>Accepted</div>
-            </div> : null
-        }
+                <div className='header'>Date:</div>
+                <div className='text'>22 December, 2023</div>
+            </div>
+            <div className='DetailCon'>
+                <div className='header'>Time:</div>
+                <div className='text'>8:50 pm</div>
+            </div>
+            <div className='DetailCon'>
+                <div className='header'>Bank Name:</div>
+                <div className='text'>First Last Name</div>
+            </div>
+            <div className='DetailCon'>
+                <div className='header'>Bank Address:</div>
+                <div className='text'>House # 12, Street # 34 Block A Lahore Punjab</div>
+            </div>
+            <div className='DetailCon'>
+                <div className='header'>Bank Admin Name:</div>
+                <div className='text'>First Second Name</div>
+            </div>
+            <div className='DetailCon'>
+                <div className='header'>Bank Admin CNIC:</div>
+                <div className='text'>35202-1234567-0</div>
+            </div>
+            <div className='DetailCon'>
+                <div className='header'>Phone Number 1: </div>
+                <div className='text'>12345678910</div>
+            </div>
+            <div className='DetailCon'>
+                <div className='header'>Phone Number 2: </div>
+                <div className='text'>12345678910</div>
+            </div>
+            <div className='DetailCon'>
+                <div className='header'>Email: </div>
+                <div className='text'>bank@gmail.com</div>
+            </div>
+            {
+                status === 'reject' ? 
+                <div className='DetailCon'>
+                    <div className='header'>Request Status: </div>
+                    <div className='text Dstatus'>Rejected</div>
+                </div> :
+                status === 'accept' ? 
+                <div className='DetailCon'>
+                    <div className='header'>Request Status: </div>
+                    <div className='text Astatus'>Accepted</div>
+                </div> : null
+            }
+            </div>
+        </div>
         <div className='heading'>Invoice Details</div>
         <div className='tableCon'>
             <table className='table'>
@@ -126,13 +139,6 @@ export default function EachBankReply() {
                 </div>
             </div>
         </div>
-        {
-            status === 'notSet' ?
-            <div className='btnCon1'>
-                <button className='RBtn' onClick={(e) => setShowDenyModal(!showDenyModal)}>Reject</button>
-                <button className='ABtn' onClick={(e) => setStatus('accept')}>Accept</button>
-            </div> : null
-        }
         <div className='logoutModal' style={{display: showDeleteModal ? 'flex' : 'none'}} onClick={(e) => setShowDeleteModal(false)}>
             <div className='logout'>
                 <div className='modalHeading'>Confirm Delete</div>
